ff06f0fb72d05f8ee8bac609c94040b545ca0799
Csilla Csaky <csilla0210@users.noreply.github.com>
2024-03-05 11:28:18 +0100

    Exercise JS with TDD

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  if (fahrenheit === 32) {
+    return 0;
+  }
+  return Number(((fahrenheit - 32) * 5 / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  if (celsius === 0) {
+    return 32;
+  }
+  return Number(((celsius * 9 / 5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
